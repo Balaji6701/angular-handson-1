b0e2900632ca61943abf9d11ff6488729301e469
Balaji6701 <balabalaji6701@gmail.com>
2022-04-01 19:29:37 +0530

    3 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
-const routes: Routes = [{ path: 'viewEmp', component: ViewEmpComponent }];
+import { EditEmpComponent } from './edit-emp/edit-emp.component';
+
+const routes: Routes = [
+  { path: 'viewEmp', component: ViewEmpComponent },
+  { path: 'editEmp', component: EditEmpComponent },
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
+import { EditEmpComponent } from './edit-emp/edit-emp.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, ViewEmpComponent],
+  declarations: [AppComponent, ViewEmpComponent, EditEmpComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
