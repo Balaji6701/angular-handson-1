4d28e0e2706c86c0f8e791e1b7ebcde638749cf0
Balaji6701 <balabalaji6701@gmail.com>
2022-04-01 19:40:19 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
 import { EditEmpComponent } from './edit-emp/edit-emp.component';
+import { SimpleComponent } from './simple/simple.component';
 
 const routes: Routes = [
   { path: 'viewEmp', component: ViewEmpComponent },
   { path: 'editEmp', component: EditEmpComponent },
+  { path: 'simple', component: SimpleComponent },
 ];
 
 @NgModule({
