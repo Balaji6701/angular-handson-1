457e92b9375fdd30a73d4ebbb281203cdbc7a2fc
Balaji6701 <balabalaji6701@gmail.com>
2022-04-02 21:56:07 +0530

    3 files modified

--- src/app/edit-emp-reactive/edit-emp-reactive.component.ts
+++ src/app/edit-emp-reactive/edit-emp-reactive.component.ts
@@ -8,8 +8,12 @@ import { Employee } from '../employee';
   styleUrls: ['./edit-emp-reactive.component.css'],
 })
 export class EditEmpReactiveComponent implements OnInit {
-  name = new FormControl('John');
   empForm: FormGroup;
+  departments = [
+    { id: 1, name: 'Payroll' },
+    { id: 2, name: 'Internal' },
+    { id: 3, name: 'HR' },
+  ];
   employee: Employee = {
     id: 1001,
     name: 'John',
@@ -24,6 +28,16 @@ export class EditEmpReactiveComponent implements OnInit {
     dateOfBirth: new Date('12/31/2000'),
   };
   constructor() {}
+  get name() {
+    return this.empForm.get('name') as FormControl;
+  }
+  get department() {
+    return this.empForm.get('department');
+  }
+
+  get salary() {
+    return this.empForm.get('salary') as FormControl;
+  }
 
   ngOnInit() {
     this.empForm = new FormGroup({
@@ -32,6 +46,9 @@ export class EditEmpReactiveComponent implements OnInit {
         Validators.minLength(4),
         Validators.maxLength(20),
       ]),
+      department: new FormControl(this.employee.department.id),
+      salary: new FormControl(this.employee.salary),
     });
+    console.log(this.employee.department.id);
   }
 }
