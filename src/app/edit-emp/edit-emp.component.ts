d5e07ad5e7259335123432c980eca8b04f9b7ca4
Balaji6701 <balabalaji6701@gmail.com>
2022-04-02 18:05:01 +0530

    2 files modified

--- src/app/edit-emp/edit-emp.component.ts
+++ src/app/edit-emp/edit-emp.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-edit-emp',
   templateUrl: './edit-emp.component.html',
-  styleUrls: ['./edit-emp.component.css']
+  styleUrls: ['./edit-emp.component.css'],
 })
 export class EditEmpComponent implements OnInit {
+  employee: Employee = {
+    id: 1001,
+    name: 'John',
+    salary: 675000,
+    department: { id: 3451, name: 'CDE' },
+    permanent: true,
+    skill: [
+      { id: 1, name: 'HTML' },
+      { id: 2, name: 'CSS' },
+      { id: 3, name: 'JAVASCRIPT' },
+    ],
+    dateOfBirth: new Date('12/31/2000'),
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
